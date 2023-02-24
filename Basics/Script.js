4d251117e9d1c125e7003165895d9561c1482f8d
Aditya Titame <adityatitame@nimapinfotech.com>
2023-02-24 15:58:10 +0530

    Basic javascript coading

---
+++ Basics/Script.js
@@ -0,0 +1,58 @@
+// let score = 40;
+// let result = score>=70 ? "Pass" : "Fail";
+// console.log(result)
+
+// Exercise 1
+// function maxNum(a,b)
+// {
+//     if(a>b)
+//     {
+//         return a;
+//     }
+//     else if(b>a)
+//     {
+//         return b;
+//     }
+//     else
+//     {
+//         return "Same value";
+//     }
+// }
+// console.log("Maximum number bet " + a + "& "+ b + "is " + maxNum(7,10));
+
+// Exercise 2
+// function checkResolution(height, width)
+// {
+//    height>width ? console.log("Potrait") : console.log("Landscape");
+// }
+// checkResolution(200,500)
+
+// Exercise 3
+// function fizzBuzz(input)
+// {
+//     if(typeof input !== 'number') return "Not a number";
+//     else if((input % 3 == 0) && (input % 5 ==0)) return "fizzBuzz";
+//     else if(input % 3 == 0) return "fizz";
+//     else if(input % 5 == 0) return "Buzz";
+//     else return input;
+// }
+// let result = fizzBuzz(7);
+// console.log(result);
+
+// Factory Functions
+// function createCircle(radius){
+// return {
+//     radius ,
+//     isVisible : true ,
+//     draw(){
+//         console.log("Draw")
+//     }
+// }
+// }
+// const circle1=createCircle(1);
+// console.log(circle1)
+// const circle2 = createCircle(2)
+// console.log(circle2)
+
+
+// Constructor Function
---
+++ Basics/factory.js
@@ -0,0 +1,8 @@
+const circle1 = {
+    radius : 1 ,
+    isVisible : true ,
+    draw : function(){
+        console.log("Draw")
+    }
+}
+console.log(circle1)
