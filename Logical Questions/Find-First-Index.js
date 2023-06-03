1d1b8899e63a734f044db8d3287c38d2a849b5b0
AdityaNimap <126067212+AdityaNimap@users.noreply.github.com>
2023-06-03 16:40:32 +0530

    Create Find-First-Index.js

---
+++ Logical Questions/Find-First-Index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+let haystack = "sadbutsad" 
+let needle = "sad"
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
