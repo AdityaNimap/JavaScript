67b871710667703f51d1e4650f00e9efa5554877
AdityaNimap <126067212+AdityaNimap@users.noreply.github.com>
2023-06-03 15:27:17 +0530

    Create Remove-Duplicates.js

---
+++ Logical Questions/Remove-Duplicates.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let nums = [0,0,1,1,1,2,2,3,3,4]
+var removeDuplicates = function(nums) {
+    let i = 0;
+    while(i<nums.length){
+        if(nums[i]=== nums[i+1]){
+            nums.splice(i,1)
+        }
+        else{
+            i++;
+        }
+    }
+};
